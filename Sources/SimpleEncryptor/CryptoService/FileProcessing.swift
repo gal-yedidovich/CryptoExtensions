6f2849a8ec2df565e4ed1e0d07d338a5fce9c7bf
Gal <gal.yedidovich@is.com>
2021-12-05 19:10:29 +0200

    MOVED AsyncChunkedSequence to separate file

--- Sources/SimpleEncryptor/CryptoService/FileProcessing.swift
+++ Sources/SimpleEncryptor/CryptoService/FileProcessing.swift
@@ -94,37 +94,3 @@ enum ProccessingError: LocalizedError {
 		}
 	}
 }
-
-@available(macOS 12.0, iOS 15.0, *)
-extension AsyncSequence {
-	func chunked(countOf chunkSize: Int) -> AsyncChunkedSequence<Self> {
-		AsyncChunkedSequence(sequence: self, chunkSize: chunkSize)
-	}
-}
-
-@available(macOS 12.0, iOS 15.0, *)
-struct AsyncChunkedSequence<AsyncSeq : AsyncSequence>: AsyncSequence {
-	typealias Element = [AsyncSeq.Element]
-	
-	let sequence: AsyncSeq
-	let chunkSize: Int
-	
-	func makeAsyncIterator() -> AsyncIterator {
-		AsyncIterator(innerIterator: sequence.makeAsyncIterator(), chunkSize: chunkSize)
-	}
-	
-	struct AsyncIterator: AsyncIteratorProtocol {
-		var innerIterator: AsyncSeq.AsyncIterator
-		let chunkSize: Int
-		
-		mutating func next() async throws -> Element? {
-			var chunk: Element = []
-			
-			while chunk.count < chunkSize, let value = try await innerIterator.next() {
-				chunk.append(value)
-			}
-			
-			return chunk.isEmpty ? nil : chunk
-		}
-	}
-}
